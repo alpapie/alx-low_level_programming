935368f2350ce65d0c3549aa598be891fec24b09
alpapie <alpapie0908@gmail.com>
2022-06-04 15:28:39 +0000

    commit first loop program

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,9 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/* more headers goes there 
+*jsjsjdjjjdjjsjd
+* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
