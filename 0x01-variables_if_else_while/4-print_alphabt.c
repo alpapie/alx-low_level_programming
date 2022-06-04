1e35fbdaf637b8816a2dced915a7b3f916575d05
alpapie <alpapie0908@gmail.com>
2022-06-04 17:53:53 +0000

    commit first loop program

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,20 +2,20 @@
 #include <stdio.h>
 
 /**
-* main - to tell if a random number is either - or +  
-*   
-*n is the random integer  
-*  
-*Return: the integer  
-*/
+ * main - to tell if a random number is either - or +
+ *
+ * n is the random integer
+ *
+ * Return: the integer
+ */
 int main(void)
 {
 char cha = 'a';
 
-while(cha <= 'z')
+while (cha <= 'z')
 {
-	if(cha != 'e' && cha != 'q')
-		putchar( cha);
+	if (cha != 'e' && cha != 'q')
+		putchar(cha);
         cha++;
 }
 putchar('\n');
