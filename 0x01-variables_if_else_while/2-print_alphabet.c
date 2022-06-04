ccbb2829cebf23f4df5b793167e9f511282900e1
alpapie <alpapie0908@gmail.com>
2022-06-04 10:12:29 -0700

    dds

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
+
 /*
-    * betty style doc for function main goes there
+* betty style doc for function main goes there
 */
 int main(void)
 {
-    char cha='a';
-    while(cha<='z')
-    {
-        putchar("%c", cha);
-        cha++;
-    }
-    return (0);
+char cha = 'a';
+while (cha <= 'z')
+{
+	putchar("%c", cha);
+	cha++;
+}
+return (0);
 }
