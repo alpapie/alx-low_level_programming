b6b0252031b67893cebfae081a281fe89abb8683
alpapie <alpapie0908@gmail.com>
2022-06-04 14:13:26 +0000

    commit first loop program

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,11 +12,15 @@ int main(void)
 	/* your code goes there */
     if (n > 0)
     {
-        printf("%d is positive\n", n);
+        printf("%d is positive", n);
+    }
+    else if (n < 0)
+    {
+        printf("%d is negative", n);
     }
     else
     {
-        printf("%d is negative\n", n);
+        printf("%d is zero", n);
     }
 	return (0);
 }
