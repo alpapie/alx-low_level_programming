ea4f5d5557d068713d5f3142fe1e7248423cb8a8
alpapie <alpapie0908@gmail.com>
2022-06-04 16:30:46 +0000

    commit first loop program

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,10 +2,13 @@
 #include <time.h>
 #include <stdio.h>
 
-/* more headers goes there 
-*jsjsjdjjjdjjsjd
-* betty style doc for function main goes there 
-*/
+/**
+ * main - to tell if a random number is either - or +
+ *
+ * n is the random integer
+ *
+ * Return: the integer
+ */
 int main(void)
 {
 int n;
@@ -13,12 +16,12 @@ int nlast;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-nlast = n%10;
+nlast = n % 10;
 if(nlast > 5)
 	printf("Last digit of %d is %d and is greater than 5\n",n, nlast);
 else if (nlast == 0)
         printf("Last digit of %d is %d and is 0\n", n, nlast);
 else if (nlast < 6 && nlast != 0)
-        printf("Last digit of %d is %d and is less than 6 and not 0n", n, nlast);
+        printf("Last digit of %d is %d and is less than 6 and not 0\n", n, nlast);
 return (0);
 }
