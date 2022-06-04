1cce0f9aff17e66ca0aff8afe16f0dfab3328273
alpapie <alpapie0908@gmail.com>
2022-06-04 16:14:38 +0000

    commit first loop program

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,7 +4,8 @@
 
 /* more headers goes there 
 *jsjsjdjjjdjjsjd
-* betty style doc for function main goes there */
+* betty style doc for function main goes there 
+*/
 int main(void)
 {
 int n;
@@ -12,11 +13,11 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 n=n%10;
-if(n>5)
+if(n > 5)
 	printf("Last digit of n is %d  and is greater than 5\n", n);
-else if (n==0)
+else if (n == 0)
         printf("Last digit of n is %d and is 0\n", n);
-else if (n> 6 && n!=0)
+else if (n < 6 && n != 0)
         printf("Last digit of n is %d and is less than 6 and not 0n", n);
 return (0);
 }
