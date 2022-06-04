3e437d12f702a91f053b9717d70b0ac3a4379923
alpapie <alpapie0908@gmail.com>
2022-06-04 15:39:09 +0000

    commit first loop program

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,8 +13,8 @@ int main(void)
 {
 int n;
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 if (n > 0)
     printf("%d is positive\n", n);
 else if (n == 0)
