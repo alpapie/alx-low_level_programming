80143f395c273b398a4613f5275ade46ae3333b1
alpapie <alpapie0908@gmail.com>
2022-06-05 01:02:28 +0000

    add

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,11 +12,11 @@ int main(void)
 {
 char i;
 
-for (i ='0'; i <= '9' ; i++)
+for (i = '0'; i <= '9' ; i++)
 {
 	putchar(i);
 }
-for (i ='a'; i <= 'f' ; i++)
+for (i = 'a'; i <= 'f' ; i++)
 {
 	putchar(i);
 }
