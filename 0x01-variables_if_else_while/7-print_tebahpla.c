091ac63beff41ffd2e48b5ae64747faddbf2578e
alpapie <alpapie0908@gmail.com>
2022-06-05 00:46:40 +0000

    add

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -11,6 +11,7 @@
 int main(void)
 {
 char cha = 'z';
+
 while (cha >= 'a')
 {
     putchar(cha);
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - to tell if a random number is either - or +
+ *
+ * n is the random integer
+ *
+ * Return: the integer
+ */
+int main(void)
+{
+int i = 0;
+char cha = 'a';
+
+for (; i <= 9 ; i++)
+{
+	putchar(i);
+}
+for (; cha <= 'f' ; ch++)
+{
+	putchar(i);
+}
+printf("\n");
+}
