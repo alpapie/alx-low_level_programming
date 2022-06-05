73a36cc220bf9f328abe67e1b9917e573d834eab
alpapie <alpapie0908@gmail.com>
2022-06-05 01:06:19 +0000

    add

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - outputs decimal numbers to 9
+ * with space and comma.
+ *
+ *
+ *
+ * Return: the integer(0 success)
+ */
+
+int main(void)
+{
+int number;
+
+for (number = '0'; number <= '9'; number++)
+{
+    putchar(number);
+    if (number != '9')
+    {
+        putchar(',');
+        putchar(' ');
+    }
+}
+putchar('\n');
+return (0);
+}
