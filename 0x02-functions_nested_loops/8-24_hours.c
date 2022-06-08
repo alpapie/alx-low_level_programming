98e6a1bf2f37df741bd3e2a08d205c6cdaad91f6
alpapie <alpapie0908@gmail.com>
2022-06-08 00:49:05 +0000

    aallal amjahn

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,29 +4,20 @@
  * jack_bauer - prints a 24 hour timer
  */
 
-void jack_bauer(void)
+oid jack_bauer(void)
 {
-int hour=0;
-int minute;
+	int i, j;
 
-for (; hour <= 23 ; hour++)
-{
-	if (minute<10)
+	for (i = 0; i < 24; i++)
 	{
-		_putchar("0");
-	}
-    _putchar(minute);
-	_putchar(hour);
-    _putchar(":");
-	minute=0;
-	for (;minute <60 ; minute++)
-	{
-		if (minute<10)
+		for (j = 0; j < 60; j++)
 		{
-			_putchar("0");
+			_putchar(i / 10 + 48);
+			_putchar(i % 10 + 48);
+			_putchar(':');
+			_putchar(j / 10 + 48);
+			_putchar(j % 10 + 48);
+			_putchar('\n');
 		}
-		_putchar(minute);
 	}
-	_putchar("\n");
-}
 }
