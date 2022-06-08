cba44879520a2e078cabe7cbcdc7cc72e8db50aa
alpapie <alpapie0908@gmail.com>
2022-06-08 00:05:27 +0000

    aallal amjahn

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,6 +9,18 @@
 
 int print_last_digit(int n)
 {
-return (n%10);
+int last_digit;
+
+last_digit = n % 10;
+
+if (last_digit < 0)
+{
+	last_digit = last_digit * -1;
+	_putchar(last_digit);
+}
+
+_putchar(last_digit + '0');
+
+return (last_digit);
 }
 
