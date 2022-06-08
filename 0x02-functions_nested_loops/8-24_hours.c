f2bdf6046a26c0cc1da76f544778efdf5026859f
alpapie <alpapie0908@gmail.com>
2022-06-08 00:44:20 +0000

    aallal amjahn

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,26 +7,26 @@
 void jack_bauer(void)
 {
 int hour=0;
-int minute
+int minute;
 
 for (; hour <= 23 ; hour++)
 {
 	if (minute<10)
 	{
-		_putchar("0")
+		_putchar("0");
 	}
-    _putchar(minute)
+    _putchar(minute);
 	_putchar(hour);
-    _putchar(":")
+    _putchar(":");
 	minute=0;
 	for (;minute <60 ; minute++)
 	{
 		if (minute<10)
 		{
-			_putchar("0")
+			_putchar("0");
 		}
-		_putchar(minute)
+		_putchar(minute);
 	}
-	_putchar("\n")
+	_putchar("\n");
 }
 }
