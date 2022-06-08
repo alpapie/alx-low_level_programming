551f49fc5bbb559120f0c0cc8d5dc595fd9d7ac9
alpapie <alpapie0908@gmail.com>
2022-06-08 01:03:39 +0000

    aallal amjahn

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,16 +1,39 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - to tell if a random number is either - or +
+ * times_table - from 0-9
  *
- * n is the random integer
- *
- * Return: the integer
+ * Description: prints all the nine table
+ * Return: Always (0).
  */
-int main(void)
-{
 
-return (0);
+void times_table(void)
+{
+	int i, j, product;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (j == 0)
+			{
+				_putchar(48);
+				continue;
+			}
+			product = i * j;
+			_putchar(',');
+			_putchar(' ');
+			if (product >= 10)
+			{
+				_putchar(product / 10 + 48);
+				_putchar(product % 10 + 48);
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(product + 48);
+			}
+		}
+		_putchar('\n');
+	}
 }
