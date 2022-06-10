19975ca237654826baae906734dd5ecda6e296e9
alpapie <alpapie0908@gmail.com>
2022-06-10 00:12:51 +0000

    changement

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,9 +1,22 @@
 #include "main.h"
 
 /**
- * largest_number - returns the largest of 3 numbers
- * @a: first integer
- * @b: second integer
- * @c: third integer
- * Return: largest number
+ *print_most_numbers - returns the largest of 3 numbers
  */
+
+void more_numbers(void)
+{
+int i;
+int j;
+
+for (i = 0; i <= 9; i++){
+	for (i = 0; i <= 14; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
+	}
+}
+_putchar('\n');
+}
