2d2112acbe79f39816c92ebc12f2eaab5f124a72
alpapie <alpapie0908@gmail.com>
2022-06-10 00:53:36 +0000

    changement

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,14 +7,14 @@
 
 void more_numbers(void)
 {
-int i, j=0;
+int i;
+int j;
 
-while (j <= 10; ){
-	for (; i <= 14; i++)
+for (j = 0; j <= 10; j++){
+	for (i = 0; i <= 14; i++)
 	{
 		_putchar(i + '0');
 	}
-	j++
 }
 _putchar('\n');
 }
