f8ed31613b668684b35c3545ccaf5cb9dab249f9
alpapie <alpapie0908@gmail.com>
2022-06-10 00:38:04 +0000

    changement

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,18 +2,19 @@
 
 /**
  *more_numbers - prints all numbers from 0 to 14 for 10 times
+ * Return: void
  */
 
 void more_numbers(void)
 {
-int i;
-int j;
+int i=0, j=0;
 
-for (j = 0; j <= 10; j++){
-	for (i = 0; i <= 14; i++)
+while (; j <= 10; ){
+	for (; i <= 14; i++)
 	{
 		_putchar(i + '0');
 	}
+	j++
 }
 _putchar('\n');
 }
