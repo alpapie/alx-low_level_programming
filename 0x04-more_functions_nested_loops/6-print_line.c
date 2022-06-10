32b995b33868c078955b9aebd2e7a8b7f6542811
alpapie <alpapie0908@gmail.com>
2022-06-10 01:52:25 +0000

    changement

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,8 +1,18 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints all aphabets in lowercase
- *
- *
- *
+ *more_numbers - prints all numbers from 0 to 14 for 10 times
+ *@c: character to print
+ * Return: void
  */
+
+void print_line(int n)
+{
+int i=1;
+
+while (i <= n)
+{
+	_putchar("_");
+}
+_putchar('\n');
+}
