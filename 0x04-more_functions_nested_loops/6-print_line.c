e7d0dd2ce81e39e9d4ba84a09c6327dd8f68fab4
alpapie <alpapie0908@gmail.com>
2022-06-10 02:05:47 +0000

    changement

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,19 +1,22 @@
 #include "main.h"
 
 /**
- *more_numbers - prints all numbers from 0 to 14 for 10 times
- *@c: character to print
- * Return: void
+ * print_line - Print line dependent on the input
+ * @n : The number of '_' characters
+ * Return: Void.
  */
 
 void print_line(int n)
 {
 int i=1;
 
-while (i <= n)
+if (n > n)
 {
-	_putchar("_");
-	i++;
+	while (i <= n)
+	{
+		_putchar('_');
+		i++;
+	}
 }
 _putchar('\n');
 }
