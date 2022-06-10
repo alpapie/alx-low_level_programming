b3bf8d7c91a55ed471208598705f317e91d33b39
alpapie <alpapie0908@gmail.com>
2022-06-10 02:18:36 +0000

    changement

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * print_diagonal- Print line dependent on the input
+ * @n : The number of '_' characters
+ * Return: Void.
+ */
+
+void print_diagonal(int n)
+{
+int i = 1;
+if (n > 0)
+{
+	while (i <= n)
+	{
+		int j = 1;
+		while (j <= i)
+		{
+			_putchar(' ');
+			j++;
+		}
+		_putchar('\\');
+		_putchar('\n');
+		i++;
+	}
+}
+_putchar('\n');
+}
+{
+
+}
+}
