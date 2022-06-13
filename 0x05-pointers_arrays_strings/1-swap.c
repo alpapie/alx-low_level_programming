51d097bb9a7daea188b556d8b40d624bb2e6541a
alpapie <alpapie0908@gmail.com>
2022-06-13 19:40:12 +0000

    changement

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int- returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+
+void swap_int(int *a, int *b)
+{
+int *tempon = a;
+*a = *b;
+*b = *tempon;
+}
