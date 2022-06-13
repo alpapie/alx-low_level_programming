6fea3ca297ce78cb54c668706940ffc16aa4b473
alpapie <alpapie0908@gmail.com>
2022-06-13 20:28:08 +0000

    changement

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+/**
+ * rev_string - returns the largest of 3 numbers
+ * @s: first integer
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+
+void rev_string(char *s)
+{
+int i, j, temp;
+
+i = 0;
+j = _strlen(s) - 1;
+while (i < j)
+{
+	temp = s[i];
+	s[i] = s[j];
+	s[j] = temp;
+	i++;
+	j--;
+}
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -32,3 +32,4 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
+void rev_string(char *s);
