280b6cb6b3e6fabef662ab69c7f91aea39a75864
alpapie <alpapie0908@gmail.com>
2022-06-13 19:49:15 +0000

    changement

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _puts - returns the largest of 3 numbers
+ * @str: first integer
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+puts(*str);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -30,3 +30,4 @@ void print_triangle(int size);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void _puts(char *str);
