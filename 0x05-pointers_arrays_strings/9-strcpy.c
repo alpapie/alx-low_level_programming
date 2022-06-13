1e4af99d626c5b029829c0cd2b0e707517ffc88b
alpapie <alpapie0908@gmail.com>
2022-06-13 22:10:48 +0000

    changement

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcpy - returns the largest of 3 numbers
+ * @src: first integer
+ * @dest: second integer
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+dest = &src;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -35,3 +35,4 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts_half(char *str);
 void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
