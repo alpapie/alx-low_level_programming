1d7360ae09710a0dc2850db69ac070d1bea3a9b2
alpapie <alpapie0908@gmail.com>
2022-06-13 20:38:27 +0000

    changement

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * puts2 - returns the largest of 3 numbers
+ * @str: first integer
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+
+void puts2(char *str)
+{
+int i = 0;
+int lenn = strlen(str);
+
+for (; i >= 0; i++)
+{
+	if (s[i] == '\0')
+	{
+		break;
+	}
+	_putchar(s[i]);
+	_putchar('\n');
+}
+}
