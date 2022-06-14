72d29fc23a97b007d7ee1d3e184b09473af7e3bb
alpapie <alpapie0908@gmail.com>
2022-06-14 01:26:18 +0000

    changement

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,6 +10,49 @@
 
 int main(void)
 {
-printf("Tada! Congrats");
+int i = 0;
+int randomizer = 0;
+
+srand((unsigned int)(time(NULL)));
+char numbers[] = "0123456789";
+char letter[] = "abcdefghijklmnoqprstuvwyzx";
+
+// Array of capital alphabets
+char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
+
+// Array of all the special symbols
+char symbols[] = "!@#$^&*?";
+
+// Stores the random password
+char password[N];
+
+// To select the randomizer
+// inside the loop
+randomizer = rand() % 4;
+
+// Iterate over the range [0, N]
+for (i = 0; i < N; i++) {
+
+    if (randomizer == 1) {
+        password[i] = numbers[rand() % 10];
+        randomizer = rand() % 4;
+        printf("%c", password[i]);
+    }
+    else if (randomizer == 2) {
+        password[i] = symbols[rand() % 8];
+        randomizer = rand() % 4;
+        printf("%c", password[i]);
+    }
+    else if (randomizer == 3) {
+        password[i] = LETTER[rand() % 26];
+        randomizer = rand() % 4;
+        printf("%c", password[i]);
+    }
+    else {
+        password[i] = letter[rand() % 26];
+        randomizer = rand() % 4;
+        printf("%c", password[i]);
+    }
+}
 return (0);
 }
