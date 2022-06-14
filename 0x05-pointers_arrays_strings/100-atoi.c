175f5eb14fe331b8b093bf5854621d466e17b384
alpapie <alpapie0908@gmail.com>
2022-06-14 01:00:35 +0000

    changement

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,29 +2,29 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * puts2 - returns the largest of 3 numbers
- * @str: first integer
- * Return: 1 if c is uppercase, 0 otherwise
- */
+ * _atoi - returns the largest of 3 numbers
+ * @s: first integer
+ * Return:int
 
 int _atoi(char *s)
 {
-int i = 0, n = 0;
 int sign = 1;
+unsigned int num = 0;
 
-while (s[i] == ' ' || (s[i] >= '\t' && s[i] <= '\r'))
-	i++;
-if (s[i] == '-')
-{
-	sign = -1;
-	i++;
-}
-else if (s[i] == '+')
-	i++;
-while (s[i] >= '0' && s[i] <= '9')
-{
-	n = n * 10 + (s[i] - '0');
-	i++;
-}
-return (n * sign);
+do {
+
+	if (*s == '-')
+	{
+		sign *= -1;
+	}
+	else if (*s >= '0' && *s <= '9')
+	{
+		num = (num * 10) + (*s - '0');
+	}
+	else if (num > 0)
+	{
+		break;
+	}
+} while (*s++);
+return (num * sign);
 }
