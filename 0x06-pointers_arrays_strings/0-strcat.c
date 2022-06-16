31c72a70e2cc4af555cca7d066ef29c6e46c0196
alpapie <alpapie0908@gmail.com>
2022-06-16 23:41:21 +0000

    changement

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,1 +1,16 @@
-#include <'main'>
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *_strcat - returns the largest of 3 numbers
+ * @dest: first integer
+ * @src: second integer
+ * Return: char 
+ */
+
+char *_strcat(char *dest, char *src)
+{
+strcat(dest, src);
+return (dest);
+}
