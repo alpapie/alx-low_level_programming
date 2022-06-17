f6fe5817ceb6311fb42b473ef7bf0d0312992be2
alpapie <alpapie0908@gmail.com>
2022-06-17 00:13:09 +0000

    changement

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *_strcmp - returns the largest of 3 numbers
+ * @s1: first integer
+ * @s2: second integer
+ * Return: n
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+int result ;
+
+result = strcmp(s1,s2)
+return (result);
+}
