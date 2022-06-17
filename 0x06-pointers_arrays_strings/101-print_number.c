27adae065b4310377f00356d431b26750f7c6126
alpapie <alpapie0908@gmail.com>
2022-06-17 02:05:46 +0000

    changement

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,6 +8,19 @@
 
 void print_number(int n)
 {
-    printf("%d\n", n);
-    
+unsigned int n1;
+
+n1 = n;
+
+if (n < 0)
+{
+	_putchar('-');
+	n1 = -n;
+}
+
+if (n1 / 10 != 0)
+{
+	print_number(n1 / 10);
+}
+_putchar((n1 % 10) + '0');
 }
