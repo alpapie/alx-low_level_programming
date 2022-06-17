d22588f386f4deea491cc1211d947cf8482aa462
alpapie <alpapie0908@gmail.com>
2022-06-17 00:00:31 +0000

    changement

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,6 +12,21 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-strcat(dest, src[n]);
-return (dest);
+int i, j;
+char *p;
+
+p = dest;
+
+for (i = 0; dest[i] != '\0'; i++)
+{
+	;
+}
+
+for (j = 0; j < n && src[j] != '\0'; j++)
+{
+	dest[i + j] = src[j];
+}
+dest[i + j] = '\0';
+
+return (p);
 }
