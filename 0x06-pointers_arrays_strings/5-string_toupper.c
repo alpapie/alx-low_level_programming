83ec2631a800416aab4642c09fdfcff429d47b47
alpapie <alpapie0908@gmail.com>
2022-06-17 01:18:45 +0000

    changement

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,9 +1,8 @@
 #include "main.h"
-#include <ctype.h> 
 #include <string.h>
 
 /**
- *string_toupper - returns the largest of 3 numbers
+ * string_toupper - returns the largest of 3 numbers
  * @s: first integer
  * Return: char
  */
