33e9b09c4f309afd69e0a60460f8ec5aef5f66f5
alpapie <alpapie0908@gmail.com>
2022-06-17 02:00:24 +0000

    changement

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,9 +2,9 @@
 #include <string.h>
 
 /**
- *_strcmp - returns the largest of 3 numbers
- * @s1: first integer
- * @s2: second integer
+ * _strcmp - returns the largest of 3 numbers
+ * @s1  : first integer
+ * @s2 : second integer
  * Return: 0 if equal, 1 if s1 > s2, -1 if s1 < s2
  */
 
@@ -12,6 +12,6 @@ int _strcmp(char *s1, char *s2)
 {
 int result;
 
-result = strcmp(s1,s2);
+result = strcmp(s1, s2);
 return (result);
 }
