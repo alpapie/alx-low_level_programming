563a2338c3b447425a48b4252683f8b5dbaca74d
alpapie <alpapie0908@gmail.com>
2022-06-20 22:21:45 +0000

    changement

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,17 +1,18 @@
 #include "main.h"
-#include <stdio.h>
-#include <string.h>
 
 /**
  *_memset - returns the largest of 3 numbers
  * @s: first integer
  * @b: second integer
  * @n: third
- * Return: dest
+ * Return: s
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-memset(s, b, n);
+unsigned int i;
+
+for (i = 0; i < n; i++)
+    s[i] = b;
 return (s);
 }
