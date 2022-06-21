75c683439d1e6292442a8baa07b26bfd81028493
alpapie <alpapie0908@gmail.com>
2022-06-21 22:06:20 +0000

    wfc main

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,26 +1,27 @@
-#include "main.h"
+#include "holberton.h"
+#include <stdio.h>
 
 /**
- *_strpbrk - returns the largest of
- * @s: first integer
- * @accept: second integer
- * Return: dest
+ * _strpbrk - bytes
+ * @s: pointer to char
+ * @accept: pointer to char
+ * Return: NULL
  */
 
 char *_strpbrk(char *s, char *accept)
 {
-int i;
+	int i;
 
-while (*s)
-{
-    for (i = 0; accept[i]; i++)
-    {
-        if (*s == accept[i])
-        {
-            return (s);
-        }
-    }
-    s++;
-}
-return (NULL);
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+	return (NULL);
 }
