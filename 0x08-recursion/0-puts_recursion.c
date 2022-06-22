5f0266512d7b7343b0bb02182940bb8b0be6af8e
alpapie <alpapie0908@gmail.com>
2022-06-22 19:57:51 +0000

    changement

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _puts_recursion - ironnou
+ * @s: pointer to char param
+ * Return: *s
+ */
+void _puts_recursion(char *s)
+{
+puts(s);
+puts('\n');
+}
--- compil/Untitled1.cpp
+++ compil/Untitled1.cpp
@@ -1,10 +1,15 @@
 #include <stdio.h>
-#include <string.h>
 
-int main(void)
-{
-	char *s="akoaooaoieie";
-	strrev(*s);
-    puts(*s);
-	return (0);
+unsigned long long int factorial(unsigned int i) {
+
+   if(i <= 1) {
+      return 1;
+   }
+   return i * factorial(i - 1);
+}
+
+int  main() {
+   int i = 12;
+   printf("Factorial of %d is %d\n", i, factorial(i));
+   return 0;
 }
