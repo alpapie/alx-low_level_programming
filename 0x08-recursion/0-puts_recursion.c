33d61eec2a02348c5297d5d8a7a119ac69177772
alpapie <alpapie0908@gmail.com>
2022-06-22 20:08:20 +0000

    changement

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,6 +9,14 @@
  */
 void _puts_recursion(char *s)
 {
-puts(s);
-puts('\n');
+if (*s == '\0')
+{
+	_putchar('\n');
+}
+else
+{
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
+
 }
--- compil/Untitled1.cpp
+++ compil/Untitled1.cpp
@@ -1,15 +1,9 @@
 #include <stdio.h>
 
-unsigned long long int factorial(unsigned int i) {
-
-   if(i <= 1) {
-      return 1;
-   }
-   return i * factorial(i - 1);
-}
 
 int  main() {
-   int i = 12;
-   printf("Factorial of %d is %d\n", i, factorial(i));
+	char *s="alpapire is the bos"
+   puts(s);
+   puts('\n');
    return 0;
 }
