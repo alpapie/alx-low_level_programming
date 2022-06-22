73e690b64ad29898886197dee552116acb1ae211
alpapie <alpapie0908@gmail.com>
2022-06-22 22:58:05 +0000

    changement

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,24 +2,45 @@
 #include <stdio.h>
 #include <string.h>
 
+int tmp_prime(int n, int i);
+
 /**
- * is_prime_number - _sqrt_recursion
- * @n: integer paramtr
- * Return: sqrt
+ * divisors - number is prime?
+ * @n: integer params
+ * @m: integer params
+ * Return: boolean
+ */
+
+int divisors(int n, int m)
+{
+	if (m % n == 0)
+	{
+		return (0);
+	}
+	else if (m / 2 > n)
+	{
+		return (divisors(n + 2, m));
+	}
+	else
+	{
+		return (1);
+	}
+}
+
+/**
+ * is_prime_number - prime
+ * @n: integer params
+ * Return: recursion
  */
 
 int is_prime_number(int n)
 {
-int i = n / 2;
-
-if (i == 1)
-{
-	return 1;
-}
-else if (n % i == 0)
-{
-	return 0;
-}     
-i = i -1; 
-return (is_prime_number(n));
+	if ((!(n % 2) && n != 2) || n < 2)
+	{
+		return (0);
+	}
+	else
+	{
+		return (divisors(3, n));
+	}
 }
