6ef7b428f0859b007d1625b818ae76ca0a95e411
alpapie <alpapie0908@gmail.com>
2022-06-22 19:59:23 +0000

    changement

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,14 @@
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+
+void print_number(int n);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void _puts_recursion(char *s);
