8193013c1be92d21a8e026d5a794ebaf0ba9827b
alpapie <alpapie0908@gmail.com>
2022-06-22 22:50:23 +0000

    changement

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -12,17 +12,17 @@ int is_prime_number(int n)
 {
     int i = n / 2;
 
-if(i == 1)
+if (i == 1)
 {
 	return 1;
 }
-else if(n % i == 0)
+else if (n % i == 0)
 {
 	return 0;
 }     
 else
 {
 	i = i -1; 
-	checkForPrime(n1);//calling the function checkForPrime itself recursively
+	is_prime_number(n);
 }
 }
