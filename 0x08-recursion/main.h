f52acc0c92860c8ac99f073f0acaa15fee85d5bf
alpapie <alpapie0908@gmail.com>
2022-06-22 20:32:46 +0000

    changement

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,3 +9,4 @@ char *_strstr(char *haystack, char *needle);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
+int factorial(int n);
--- compil/Untitled1.cpp
+++ compil/Untitled1.cpp
@@ -1,9 +1,18 @@
 #include <stdio.h>
 
+void _print_rev_recursion(char *s)
+{
+if (*s)
+{
+	
+	putchar(*s);
+}
+}
 
 int  main() {
-	char *s="alpapire is the bos"
-   puts(s);
-   puts('\n');
-   return 0;
+char *s="alpapire is the bos";
+
+	_print_rev_recursion(s);
+
 }
+
