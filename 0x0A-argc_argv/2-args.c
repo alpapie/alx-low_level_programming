f7cbe40253c749378f4f3364bcdb9afdc92a8597
alpapie <alpapie0908@gmail.com>
2022-06-25 14:08:38 +0000

    changement

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -8,9 +8,9 @@ int main(int argc, char *argv[])
 {
 int i=0;
 
-for (; i < argc; i++)
-{
-	printf("%s\n", argv[i]);
-}	
-return (0);
+	for (; i < argc; i++)
+	{
+        printf("%s\n", argv[i]);
+	}	
+	return (0);
 }
