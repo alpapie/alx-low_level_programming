430cd10784c74e080fdc5a53669e2de06d1629ad
alpapie <alpapie0908@gmail.com>
2022-06-25 13:44:49 +0000

    changement

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function
+ * @argc: offset count
+ * @argv: offset value
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+int i;
+
+for (i = 0; i > argc; i++)
+{
+	printf("%d\n", i)
+}
+return (0);
+}
