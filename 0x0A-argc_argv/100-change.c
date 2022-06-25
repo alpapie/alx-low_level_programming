f39ab388ab0ec70d0d7bbf0008428561d5cbad80
alpapie <alpapie0908@gmail.com>
2022-06-25 22:11:15 +0000

    dernier

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,57 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 /**
- * main - prints all arguments it receives
+ * main - prints the minimum number of coins
+ * to make change for an amount of money
  * @argc: n args
  * @argv: arr args
  * Return: 0
  */
-
 int main(int argc, char *argv[])
 {
-int som = atoi(argv[1]), rendu = 0;
+	int value, c;
 
-if (argc - 1 != 1)
-{
-	printf("Error\n");
-	return (1);
-}
-else
-{
-    if (som <= 0)
-    {
-        printf("%d\n",rendu);
-        return (0);
-    }
-    else
-    {
-        if (som >= 25)
-        {
-            rendu += som/25;
-            som = som %25;
-        }
-        if (som >= 10)
-        {
-            rendu += som/10;
-            som = som %10;
-        }
-        if (som >= 5)
-        {
-            rendu += som/5;
-            som = som %5;
-        }
-        if (som >=2 )
-        {
-            rendu += som/2;
-            som = som %2;
-        }
-        if (som >= 1 )
-        {
-            rendu += som/1;
-        }
-    }
-}
-printf("%d\n",rendu);
-return (0);
+	c = 0;
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	value = atoi(argv[1]);
+	if (value < 0)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+	if (value % 25 >= 0)
+	{
+		c += value / 25;
+		value = value % 25;
+	}
+	if (value % 10 >= 0)
+	{
+		c += value / 10;
+		value = value % 10;
+	}
+	if (value % 5 >= 0)
+	{
+		c += value / 5;
+		value = value % 5;
+	}
+	if (value % 2 >= 0)
+	{
+		c += value / 2;
+		value = value % 2;
+	}
+	if (value % 1 >= 0)
+	{
+		c += value / 1;
+	}
+	printf("%d\n", c);
+	return (0);
 }
