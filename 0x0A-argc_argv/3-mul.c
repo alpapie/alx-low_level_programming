ec3d4f427379d0cb5d435e53615c3b21f688c7ee
alpapie <alpapie0908@gmail.com>
2022-06-25 14:42:12 +0000

    changement

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,19 +10,17 @@
 
 int main(int argc, char *argv[])
 {
-int i, mult = 1;
+	int i, val = 1;
 
-if (argc - 1 >= 2)
-{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
 	for (i = 1; i < argc; i++)
 	{
-		mult *= atoi(argv[i]);
+		val *= atoi(argv[i]);
 	}
-}
-else
-{
-	printf("Error");
-	return (1);
-}
-return (0);
+	printf("%d\n", val);
+	return (0);
 }
