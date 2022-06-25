aec84961901d1a239f4cad3129c9b77dbb32ce98
alpapie <alpapie0908@gmail.com>
2022-06-25 21:41:48 +0000

    dernier

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -14,40 +14,40 @@ int som = atoi(argv[1]), rendu = 0;
 
 if (argc - 1 != 1)
 {
-    printf("Error\n");
-    return (1);
+	printf("Error\n");
+	return (1);
 }
 if (som < 0)
 {
-    printf("%d\n",rendu , som);
-    return (0)
+	printf("%d\n",rendu , som);
+	return (0)
 }
 else
 {
-    if (som >= 25)
-    {
-        rendu += som/25;
-        som = som %25;
-    }
-    if (som >= 10)
-    {
-        rendu += som/10;
-        som = som %10;
-    }
-        if (som >= 5)
-    {
-        rendu += som/5;
-        som = som %5;
-    }
-    if (som >=2 )
-    {
-        rendu += som/2;
-        som = som %2;
-    }
-     if (som >= 1 )
-    {
-        rendu += som/1;
-    }
+	if (som >= 25)
+	{
+		rendu += som/25;
+		som = som %25;
+	}
+	if (som >= 10)
+	{
+		rendu += som/10;
+		som = som %10;
+	}
+	if (som >= 5)
+	{
+		rendu += som/5;
+		som = som %5;
+	}
+	if (som >=2 )
+	{
+		rendu += som/2;
+		som = som %2;
+	}
+	if (som >= 1 )
+	{
+		rendu += som/1;
+	}
 
 }
 printf("%d\n",rendu);
