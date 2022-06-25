e83c6eedaf026ff21f113c0ae1eee52022d5732c
alpapie <alpapie0908@gmail.com>
2022-06-25 13:34:12 +0000

    changement

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - ironnou
+ * @argc: pointer to char param
+ * @argv[]: second variable
+ * print the name of the function
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+}
