cb146fd419cd2d556eb94af21fc5d561844965e5
alpapie <alpapie0908@gmail.com>
2022-06-25 15:19:33 +0000

    changement

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,30 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 /**
- * main - prints all arguments it receives
+ * main - adds positive numbers
  * @argc: n args
  * @argv: arr args
  * Return: 0
  */
-
 int main(int argc, char *argv[])
 {
-int i = 1, sum = 0;
+	unsigned int i, sum, num;
 
-for (; i <= argc -1; i++)
-{
-    if (atoi(argv[i]))
-    {
-        sum += atoi(argv[i]);
-    }
-    else
-    {
-        printf("Error\n");
-		return (1);
-    }
-}
-printf("%d\n", sum);
-return (sum);
+	sum = 0;
+
+	if (argc < 3)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+	while (argc-- && argc > 0)
+	{
+		for (i = 0; argv[argc][i] != '\0'; i++)
+		{
+			if (!(isdigit(argv[argc][i])))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		num = atoi(argv[argc]);
+		sum += num;
+	}
+	printf("%d\n", sum);
+	return (sum);
 }
