c6b10bf24d1496d12ec65780008221d9837d90f3
alpapie <alpapie0908@gmail.com>
2022-06-28 21:13:10 +0000

    changemeny

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,13 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-/**
- * main - prints its name, followed by a new line
- * @argc: n args
- * @argv: arr args
- * Return: 0
- */
-
 #include <stdlib.h>
 
 /**
