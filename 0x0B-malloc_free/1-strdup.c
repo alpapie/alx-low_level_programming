2c5b226a46bb92a026300e82abd8e6e7fab323d7
alpapie <alpapie0908@gmail.com>
2022-06-28 20:07:23 +0000

    changemeny

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,3 +8,20 @@
  * Return: 0
  */
 
+char *_strdup(char *str)
+{
+    char *p;
+    unsigned int i;
+
+    if (str == NULL)
+    return (0);
+    p = malloc((_strlen(str) + 1) * sizeof(char));
+    if (p == NULL)
+    return (0);
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        p[i] = str[i];
+    }
+    p[i] = '\0';
+    return (p);
+}
