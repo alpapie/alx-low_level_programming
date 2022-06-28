7783dbce20ac02c8079dedf068500d8448f8498c
alpapie <alpapie0908@gmail.com>
2022-06-28 20:05:04 +0000

    changemeny

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,10 +16,11 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 	return (0);
 	p = malloc((size) * sizeof(char));
-	if (p == '\0')
+	if (p == NULL)
 	return (0);
 	for (i = 0; i < size; i++)
 	{
 		p[i] = c;
 	}
-ret
+return (p);
+}
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints its name, followed by a new line
+ * @argc: n args
+ * @argv: arr args
+ * Return: 0
+ */
+
