c0dce49647cbbbc699028d5ed9c9a6ae863a4d08
alpapie <alpapie0908@gmail.com>
2022-06-28 19:52:05 +0000

    changemeny

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints its name, followed by a new line
+ * @argc: n args
+ * @argv: arr args
+ * Return: 0
+ */
+
+char *create_array(unsigned int size, char c)
+{
+if (size == 0)
+{
+	return (NULL)
+}
+else
+{
+	return malloc(size * sizeof(char));
+}
+}
