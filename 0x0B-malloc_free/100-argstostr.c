b6214c44e75fae765589f55985fa5e537da2fd42
alpapie <alpapie0908@gmail.com>
2022-06-28 21:19:08 +0000

    changemeny

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * _strlen - length of a string
--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-
+#include <stdlib.h>
 /**
  * strtow - char
  * @str: pointer to string params
