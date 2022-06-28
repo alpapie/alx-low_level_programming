1e2074b52af7be6545c83a2f916b39ec64463aad
alpapie <alpapie0908@gmail.com>
2022-06-28 20:03:26 +0000

    changemeny

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,17 +10,16 @@
 
 char *create_array(unsigned int size, char c)
 {
-char *p;
-unsigned int i;
+	char *p;
+	unsigned int i;
 
-if (size == 0)
-	return (NULL);
-p = malloc(size * sizeof(char));
-if (p == '\0')
-	return (NULL);
-for (int i = 0; i < size; i++)
-{
-	p[i] = c;
-}
-return c;
-}
+	if (size == 0)
+	return (0);
+	p = malloc((size) * sizeof(char));
+	if (p == '\0')
+	return (0);
+	for (i = 0; i < size; i++)
+	{
+		p[i] = c;
+	}
+ret
