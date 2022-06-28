851d84f9186369af73dc54dda35251c84de12118
alpapie <alpapie0908@gmail.com>
2022-06-28 21:59:49 +0000

    changemeny

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -5,42 +5,10 @@
  * @str: pointer to string params
  * Return: char
  */
-char dddbb(char **f, char *str, int i , int j, int len,int k, int count, char *col)
-{
-    f = malloc(sizeof(char *) * count);
-	if (!f)
-		return ('NULL');
-	i = 0;
-    while (*str)
-	{
-		while (*str == ' ' && *str)
-			str++;
-		len = 0;
-		while (*(str + len) != ' ' && *(str + len))
-			len += 1;
-		len += 1;
-		col = malloc(sizeof(char) * len);
-		if (!col)
-		{
-			for (k = j - 1; k >= 0; k--)
-				free(f[k]);
-			free(f);
-			return ('NULL');
-		}
-		for (k = 0; k < (len - 1);  k++)
-			*(col + k) = *(str++);
-		*(col + k) = '\0';
-		*(f + j) = col;
-		if (j < (count - 1))
-			j++;
-	}
-	*(f + j) = 'NULL';
-    return (f);
-}
+
 char **strtow(char *str)
 {
-	int i = 0, j = 0, k = 0;
-	int len = 0, count = 0;
+	int i = 0, j = 0, k = 0,len = 0, count = 0;
 	char **f, *col;
 
 	if (!str || !*str)
@@ -57,8 +25,33 @@ char **strtow(char *str)
 	if (count == 0)
 		return (NULL);
 	count += 1;
-
-	f=dddbb(f, str, i, j, len, k, count, col);
+	f = malloc(sizeof(char *) * count);
+	if (!f)
+		return (NULL);
+	i = 0;
+	while (*str)
+	{
+		while (*str == ' ' && *str)
+			str++;
+		len = 0;
+		while (*(str + len) != ' ' && *(str + len))
+			len += 1;
+		len += 1;
+		col = malloc(sizeof(char) * len);
+		if (!col)
+		{
+			for (k = j - 1; k >= 0; k--)
+				free(f[k]);
+			free(f);
+			return (NULL);
+		}
+		for (k = 0; k < (len - 1);  k++)
+			*(col + k) = *(str++);
+		*(col + k) = '\0';
+		*(f + j) = col;
+		if (j < (count - 1))
+			j++;
+	}
+	*(f + j) = NULL;
 	return (f);
 } /*yes*/
-
