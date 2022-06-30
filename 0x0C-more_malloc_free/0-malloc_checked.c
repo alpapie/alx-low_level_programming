281f67b50c11ee2ac8fec3425b72d0222664d08d
alpapie <alpapie0908@gmail.com>
2022-06-30 23:38:48 +0000

    changemeny

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - prints its name, followed by a new line
+ * @b: n args
+ * Return: 0
+ */
+
+void *malloc_checked(unsigned int b)
+{
+void *p;
+
+p = malloc(b);
+if (p == NULL)
+{
+    printf("Error: malloc failed\n");
+    exit(98);
+}
+return (p);
+}
