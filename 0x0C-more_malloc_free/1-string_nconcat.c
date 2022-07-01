49ae6c5dcc3f32c4956ae20c26af1dc6e432163a
alpapie <alpapie0908@gmail.com>
2022-07-01 00:05:14 +0000

    changemeny

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,38 +15,30 @@ char *p;
 unsigned int size1 = 0, size2 = 0, i;
 
 if (s1 == NULL)
-    s1 = "";
-
+	s1 = "";
 if (s2 == NULL)
-    s2 = "";
-
+	s2 = "";
 while (s1[size1] != '\0')
 {
-    size1++;
+	size1++;
 }
-
 while (s2[size2] != '\0')
 {
-    size2++;
+	size2++;
 }
-
 if (n > size2)
 n = size2;
 p = malloc((size1 + n + 1) * sizeof(char));
-
 if (p == NULL)
-    return (0);
-
+	return (0);
 for (i = 0; i < size1; i++)
 {
-    p[i] = s1[i];
+	p[i] = s1[i];
 }
-
 for (; i < (size1 + n); i++)
 {
-    p[i] = s2[i - size1];
+	p[i] = s2[i - size1];
 }
 p[i] = '\0';
-
 return (p);
 }
