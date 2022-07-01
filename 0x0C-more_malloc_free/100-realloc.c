daa8cebc091dbdddabe4903f75c266b2c72419fc
alpapie <alpapie0908@gmail.com>
2022-07-01 00:16:41 +0000

    changemeny

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _realloc -  reallocates a memory block using malloc and free
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -21,3 +21,6 @@ int _isupper(int c);
 int _isdigit(int c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int main(int argc, char *argv[]);
+int *array_range(int min, int max);
